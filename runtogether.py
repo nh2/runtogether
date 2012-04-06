bbb1d54968727647c90fa09aa515986c2bda8d33
Niklas Hambüchen <mail@nh2.me>
2012-04-07 00:21:16 +0100

    Use killTree to kill all children, no matter what they do

--- runtogether.py
+++ runtogether.py
@@ -10,7 +10,27 @@ import signal
 import subprocess
 import sys
 import time
-import atexit
+
+
+def killtree(pid, signal):
+	"""
+	Recursively kills all children of the process with given pid.
+	Source: http://stackoverflow.com/a/3211182/263061
+	"""
+	script = """
+		killTree() {
+		    local _pid=$1
+		    local _sig=${2-TERM}
+		    for _child in $(ps -o pid --no-headers --ppid ${_pid}); do
+		        killTree ${_child} ${_sig}
+		    done
+		    kill -${_sig} ${_pid}
+		}
+
+		killTree %d %s
+	""" % (pid, signal)
+
+	subprocess.call(['bash', '-c', script])
 
 
 def runtogether(commands, kill_timeout=3, shutdown_callback=None):
@@ -23,31 +43,37 @@ def runtogether(commands, kill_timeout=3, shutdown_callback=None):
 
 	procs = []
 
-	def shutdown():
+	def shutdown(exit_code):
 		print("Asking all child processes to terminate...")
+
 		for proc in procs:
 			if proc.poll() is None:
-				proc.terminate()
+				killtree(proc.pid, "TERM")
 		if any( p.poll() is None for p in procs ):
 			time.sleep(kill_timeout)
 			print("Killing remaining child processes...")
 			for proc in procs:
 				if proc.poll() is None:
-					proc.kill()
-					proc.wait()
+					killtree(proc.pid, "KILL")
 		print("done.")
 		if shutdown_callback is not None:
 			shutdown_callback()
+		sys.exit(exit_code)
 
-	atexit.register(shutdown)
-
-	signal.signal(signal.SIGTERM, lambda signum, stack_frame: shutdown())
-	signal.signal(signal.SIGINT, lambda signum, stack_frame: shutdown())
+	signal.signal(signal.SIGTERM, lambda signum, stack_frame: shutdown(130))
+	signal.signal(signal.SIGINT, lambda signum, stack_frame: shutdown(130))
 
 	# Spawn subprocesses
 	for command in commands:
 		# Try to prevent children from capturing Ctrl-C with os.setsid
-		proc = subprocess.Popen(shlex.split(command), preexec_fn=os.setsid)
+		try:
+			proc = subprocess.Popen(shlex.split(command), preexec_fn=os.setsid)
+		except:
+			import traceback
+			# Don't hide the exception
+			traceback.print_exc()
+			shutdown(1)
+		print("Launched child with pid %d: %s" % (proc.pid, command))
 		procs.append(proc)
 
 	while procs:
@@ -57,6 +83,6 @@ def runtogether(commands, kill_timeout=3, shutdown_callback=None):
 				continue
 			else:
 				procs.remove(proc)
-				print("Child terminated with exit code %d!" % ret)
-				sys.exit(ret)
+				print("Child with pid %d terminated with exit code %d!" % (proc.pid, ret))
+				shutdown(ret)
 		time.sleep(0.2)
